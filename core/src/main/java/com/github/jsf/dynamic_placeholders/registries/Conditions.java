593aa3bcf71670811b284aee099854165518e70a
Try___ <paolo.guccionemz@gmail.com>
2026-07-28 11:10:42 +0200

    fix(): Adjusted conditions

--- core/src/main/java/com/github/jsf/dynamic_placeholders/registries/Conditions.java
+++ core/src/main/java/com/github/jsf/dynamic_placeholders/registries/Conditions.java
@@ -1,12 +1,11 @@
 package com.github.jsf.dynamic_placeholders.registries;
 
-import com.github.jsf.dynamic_placeholders.components.Condition;
-import com.github.jsf.dynamic_placeholders.components.Parameter;
+import com.github.jsf.dynamic_placeholders.components.ComponentsInfo;
+import com.github.jsf.dynamic_placeholders.components.impls.conditions.Condition;
 import com.github.jsf.dynamic_placeholders.components.impls.conditions.datetime.IfDate;
 import com.github.jsf.dynamic_placeholders.names.UName;
 import com.github.utilities.validators.Preconditions;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
@@ -25,20 +24,20 @@ public class Conditions extends ComponentsRegistry<Condition> {
         return Holder.INSTANCE;
     }
 
-    public static Conditions newCondition(UName name, Function<List<Parameter<?>>, Condition> condition) {
+    public static Conditions newCondition(UName name, Function<ComponentsInfo, Condition> condition) {
         Preconditions.parameterNotNull(condition, "condition");
         getInstance().register(name, condition);
         return getInstance();
     }
 
-    public static Optional<Function<List<Parameter<?>>, Condition>> getCondition(UName name) {
+    public static Optional<Function<ComponentsInfo, Condition>> getCondition(UName name) {
         return getInstance().retrieve(Preconditions.parameterNotNull(name, "name"));
     }
 
-    public static Optional<Function<List<Parameter<?>>, Condition>> getCondition(String name) {
+    public static Optional<Function<ComponentsInfo, Condition>> getCondition(String name) {
         Preconditions.parameterNotNull(name, "name");
 
-        for (Map.Entry<UName, Function<List<Parameter<?>>, Condition>> entry : getInstance().entries()) {
+        for (Map.Entry<UName, Function<ComponentsInfo, Condition>> entry : getInstance().entries()) {
             UName actionName = entry.getKey();
 
             if (actionName.is(name)) return Optional.ofNullable(entry.getValue());
