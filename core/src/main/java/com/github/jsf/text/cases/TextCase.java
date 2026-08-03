5c0739e865ebb813dc508ee5d87ee43834b0d978
Try___ <paolo.guccionemz@gmail.com>
2026-08-03 17:39:36 +0200

    fix(): Comments

--- core/src/main/java/com/github/jsf/text/cases/TextCase.java
+++ core/src/main/java/com/github/jsf/text/cases/TextCase.java
@@ -23,16 +23,43 @@ public interface TextCase {
         return new UnknownCase();
     }
 
-
+    /**
+     * Converts the given {@code text} to space case.
+     *
+     * @param text The text to convert.
+     * @return A new array containing the parts of the text.
+     */
     String[] toSpaceCase(String text);
 
+    /**
+     * Build a string with this case pattern from the {@code text} given.
+     * <p>
+     * The text is first converted to space case and then to this case pattern.
+     *
+     * @param text The text to convert.
+     * @return A new string with this case pattern.
+     */
     default String fromSpaceCase(String text) {
         TextCase textCase = of(text);
         return fromSpaceCase(textCase.toSpaceCase(text));
     }
 
+    /**
+     * Build a string with this case pattern from the to lower case words
+     * of a text presents inside the given {@code spaceCase}
+     *
+     * @param spaceCase The array of lower case words.
+     * @return A new string with this case pattern.
+     */
     String fromSpaceCase(String[] spaceCase);
 
+    /**
+     * Splits the text in words using the given {@code delimiter}
+     *
+     * @param text The text to split
+     * @param delimiter the delimiter used to split the words
+     * @return A new array containing the parts of the text
+     */
     default String[] getWords(@NotNull String text, char delimiter) {
         if (text.isEmpty()) return new String[0];
         List<String> words = new ArrayList<>();
